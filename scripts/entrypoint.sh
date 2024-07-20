204dee014ba7b76be991a4a3887ef334cbab8699
jh1950 <rlawnsgl191@gmail.com>
2024-07-20 12:58:44 +0900

    added send a server stop signal

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -16,6 +16,14 @@ elif [ ! -d "$DATA_DIR" ]; then
 fi
 
 
+server_down() {
+	pid="$(pgrep -f "venv.*manage.py")"
+	kill -15 "$pid"
+	tail -f --pid="$pid"
+}
+trap 'server_down' 15
+
+
 ACTION "Change UID/PID"
 INFO "User UID: ${PUID}"
 INFO "User GID: ${PGID}"
@@ -25,4 +33,5 @@ groupmod -o -g "${PGID}" user > /dev/null 2>&1
 chown -R user:user "$DATA_DIR" "$VENV_DIR" /home/user
 
 
-su user -c ./main.sh
+su user -c ./main.sh &
+wait "$!"
